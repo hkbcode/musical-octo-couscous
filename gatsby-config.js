ca8f701219f84b37a64249ca37d825e4466beadf
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2020-12-19 17:16:22 +0530

    :zap: initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /* Your site config here */
+
+  siteMetadata: {
+    title: "React Developer",
+    description: "Personal blog of hemant bhoi a fullstack react developer",
+    author: "Hemant kumar Bhoi",
+  },
+
+  plugins: [
+    `gatsby-plugin-sass`,
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-relative-images`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h2>Home page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        nesciunt dignissimos pariatur labore cupiditate minima explicabo, sed
+        corrupti optio, repellat at voluptas quibusdam suscipit magnam et hic
+        eos vel voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero quod
+        molestiae consequatur. Quidem totam distinctio, ratione explicabo sequi
+        odio similique sunt sit quasi sapiente enim odit. Sed atque ipsa
+        distinctio?
+      </p>
+    </Layout>
+  )
+}
+
+export default IndexPage
